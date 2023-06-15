2451f0730036613d7881178610f0a5ccbec01e23
repmacrypn <artik3267@gmail.com>
2023-06-15 22:22:50 +0300

    header AvaName represents relevant info

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { selectIsAuth } from "../../redux/authSlice";
+import { useSelector } from "react-redux";
 
 export const Header = () => {
+    const userData = useSelector(selectIsAuth)
+    const userAvaName = userData?.firstName.slice(0, 1).toUpperCase() + userData?.lastName?.slice(0, 1).toUpperCase()
+
     return (
         <header>
             <div>
                 <Link to='/profilePage'>
-                    NK
+                    {userAvaName || 'NN'}
                 </Link>
             </div>
         </header>
