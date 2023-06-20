b036a2f050a04bbb9e7ee3571143d9a4064d5b29
repmacrypn <artik3267@gmail.com>
2023-06-20 14:24:37 +0300

    header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { selectIsAuth } from "../../redux/authSlice";
-import { useSelector } from "react-redux";
+import React from "react"
+import { Link } from "react-router-dom"
+import { selectIsAuth } from "../../redux/authSlice"
+import { useSelector } from "react-redux"
+import s from './Header.module.css'
 
 export const Header = () => {
     const userData = useSelector(selectIsAuth)
     const userAvaName = userData?.firstName.slice(0, 1).toUpperCase() + userData?.lastName?.slice(0, 1).toUpperCase()
 
     return (
-        <header>
-            <div>
-                <Link to='/profilePage'>
-                    {userAvaName || 'NN'}
-                </Link>
-            </div>
-        </header>
+        <div className={s.headerNavLink}>
+            <Link to='/profilePage'>
+                {userAvaName || 'NN'}
+            </Link>
+        </div>
     )
 }
