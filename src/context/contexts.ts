326698870b24ada31a1fe44e4cd5d366eb617e3b
repmacrypn <artik3267@gmail.com>
2023-08-08 18:50:31 +0300

    context upd

--- src/context/contexts.ts
+++ src/context/contexts.ts
@@ -1,3 +1,4 @@
 import React from 'react'
 
 export const FilterContext = React.createContext<number | null>(null)
+export const UserNameContext = React.createContext<((e: React.MouseEvent<HTMLElement>, userId: string) => void) | null>(null)
